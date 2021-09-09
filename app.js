6145cab270cf8cc55e4d81372ab38dc257d62409
Genevieve <vievenavales29@gmail.com>
2021-09-09 19:45:41 +0800

    copy button functionality

--- app.js
+++ app.js
@@ -6,21 +6,11 @@ const appDiv = document.querySelector(".app");
 
 let countResults = 0;
 
-inputField.addEventListener("keydown", (e) => {
-  if (e.keyCode === 13) {
-    getUserInput(inputField.value);
-    inputField.value = "";
-    inputField.blur();
-  }
-});
+//////////////////////////////////////////////////////////////
+// FUNCTIONS
 
-inputButton.addEventListener("click", (e) => {
-  getUserInput(inputField.value);
-  inputField.value = "";
-  inputField.blur();
-});
-
-const getUserInput = async function (userLink) {
+// Get Data from API
+const getData = async function (userLink) {
   const dataSource = await fetch(
     `https://api.shrtco.de/v2/shorten?url=${userLink}`
   );
@@ -32,11 +22,12 @@ const getUserInput = async function (userLink) {
   countResults++;
 };
 
+// Show Result to UI
 const showResult = function (userLink, shortenLink) {
   const arr = shortenLink.split(/\W/g);
-  const generateId = arr[arr.length - 1];
+  const generatedId = arr[arr.length - 1];
 
-  const html = `<div class="result negative-margin" id="${generateId}">
+  const html = `<div class="result negative-margin" id="${generatedId}">
           <h3 class="result__input heading-3">
             ${userLink}
           </h3>
@@ -45,9 +36,51 @@ const showResult = function (userLink, shortenLink) {
         </div>`;
 
   appDiv.insertAdjacentHTML("beforeend", html);
+
   if (countResults > 0) {
     document
-      .querySelector(`#${generateId}`)
+      .querySelector(`[id="${generatedId}"]`)
       .classList.remove("negative-margin");
   }
+
+  const resultDiv = document.querySelector(`[id="${generatedId}"]`);
+  copyResult(resultDiv);
 };
+
+// Copy result to Clipboard
+const copyResult = function (resultDiv) {
+  resultDiv.addEventListener("click", (e) => {
+    if (e.target.classList.contains("result__btn")) {
+      const result = resultDiv.querySelector(".result__link").innerHTML;
+      const btn = resultDiv.querySelector(".result__btn");
+
+      //copy to clipboard
+      navigator.clipboard.writeText(result);
+
+      //change button style for 3 seconds
+      btn.innerHTML = "Copied!";
+      btn.classList.add("btn-violet");
+
+      setTimeout(() => {
+        btn.innerHTML = "Copy";
+        btn.classList.remove("btn-violet");
+      }, 3000);
+    }
+  });
+};
+
+//////////////////////////////////////////////////////////////
+// EVENT LISTENERS
+inputField.addEventListener("keydown", (e) => {
+  if (e.keyCode === 13) {
+    getData(inputField.value);
+    inputField.value = "";
+    inputField.blur();
+  }
+});
+
+inputButton.addEventListener("click", () => {
+  getData(inputField.value);
+  inputField.value = "";
+  inputField.blur();
+});
