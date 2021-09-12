7e1bc129090997c9c516aecf61cbbd17eb2c949a
Genevieve <vievenavales29@gmail.com>
2021-09-12 20:40:56 +0800

    built with parcel

--- animation.js
+++ animation.js
@@ -8,12 +8,10 @@ const staticticsSection = document.querySelector(".statistics");
 document.addEventListener("scroll", () => {
   const clientHeight = document.documentElement.clientHeight;
   const sectionHeight = staticticsSection.getBoundingClientRect();
-  // console.log(clientHeight);
-  // console.log(sectionHeight);
 
   if (sectionHeight.top < clientHeight / 2) {
-    title.style.animation = "1s ease-in slideDown forwards";
-    text.style.animation = "1s ease-in 0.7s slideDown forwards";
-    cards.style.animation = "1s ease-in 1s showUp forwards";
+    title.style.animation = "0.7s ease-in slideDown forwards";
+    text.style.animation = "0.7s ease-in 0.7s slideDown forwards";
+    cards.style.animation = "0.7s ease-in 1s showUp forwards";
   }
 });
